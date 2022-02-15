60834e4f5c05ae51def3ef386c3832f9aad3d362
fyga10 <frankgiraldo62@outlook.com>
2022-02-15 17:05:25 -0500

    t0a0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array
+ * @array: The array
+ * @size: The size
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	char swapped = 'n';
+	int aux = 0;
+	size_t i, max_limit = size - 1;
+
+	if (array && size > 0)
+		do {
+			swapped = 'n';
+			for (i = 0; i < max_limit; i++)
+			{
+				if (*(array + i) > *(array + i + 1))
+				{
+					aux = *(array + i);
+					*(array + i) = *(array + i + 1);
+					*(array + i + 1) = aux;
+					print_array(array, size);
+					swapped = 'y';
+				}
+			}
+			max_limit--;
+		} while (swapped != 'n');
+}
---
+++ sort.h
@@ -0,0 +1,32 @@
+#ifndef SORT
+#define SORT
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+
+void insertion_sort_list(listint_t **list);
+void swap_forward(listint_t **head, listint_t *to_swap);
+void swap_back(listint_t **head, listint_t *to_swap);
+
+void selection_sort(int *array, size_t size);
+
+#endif 
