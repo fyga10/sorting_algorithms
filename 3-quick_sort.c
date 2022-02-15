c2855a2e4a65d7a62d0fd37148958d9eaec20807
fyga10 <frankgiraldo62@outlook.com>
2022-02-15 17:47:00 -0500

    t3a0

---
+++ 3-quick_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ *quick_sort - partition array
+ *@array: random number array
+ *@size: array size
+ *Return: the value of the pivot
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array || size)
+	{
+		return;
+	}
+}
