d42635ff6fd8a28945db4f2c96aadd3a154d5573
fyga10 <frankgiraldo62@outlook.com>
2022-02-16 08:20:08 -0500

    fix

--- sort.h
+++ sort.h
@@ -8,8 +8,8 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer in the node
- * @prev: Pointer to the previous element
- * @next: Pointer to the next element
+ * @prev: Pointer to the previous element.
+ * @next: Pointer to the next element.
  */
 typedef struct listint_s
 {
